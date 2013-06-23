faf1f8856263f58b25a3674b0a30558826c38016
PrototypingInterfaces <git@PrototypingInterfaces>
2013-06-23 03:32:23 +0200

    minor changes

--- UpdateSubtreeBranchesZipResults.sh
+++ UpdateSubtreeBranchesZipResults.sh
@@ -3,13 +3,14 @@
 
 Repos=(00_CapacitiveCover 2_VVVV-Basics 3_ArduinoBasics 4_1_Potentiometer 4_2_Temperatursensor 4_3_Accelerometer 4_4_RFID 4_5_OneDimensionalSlider 4_6_TwoDimensionalSlider 5_1_Fiducial-Tracking 5_2_AugmentedReality 5_3_Facetracking 5_4_Colortracking 5_5_Multitouch 5_6_GestureControl 6_1_Multiscreen 6_2_2DMappingOnPlaneSurfaces 6_3_3DIllusionThroughProjectionMapping 7_1_Servomotor 7_2_VibrationMotor
 )
-Branches=(master vvvv_45beta29.2)
+Branches=(master vvvv_45beta29.2 vvvv_45beta30)
 RootUrl='https://github.com/PrototypingInterfaces/'
 
 HOME='/home/PrototypingInterfaces/'
 GitRepoPath='GIT/'
 GitRepoFolder='PrototypingInterfaces_AllPatches'
 TMP='PItmp/'
+WEB='/var/www/PrototypingInterfaces'
 US='_'
 SyncOptions=" --exclude '.git*'"
 
@@ -34,6 +35,7 @@ git push origin $j --force # uncommet to push files to master remote
 	eval rsync --archive $SyncOptions $HOME$GitRepoPath$GitRepoFolder/ $HOME$TMP$GitRepoFolder$US$j/
 	pwd
 	zip -9 -r $GitRepoFolder$US$j.zip  $GitRepoFolder$US$j
-	mv -f $HOME$TMP$GitRepoFolder$US$j.zip /var/www/
+	mv -f $HOME$TMP$GitRepoFolder$US$j.zip $WEB
+
 	rm -rf $HOME$TMP
 done
