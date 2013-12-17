ef6009396c291b47a5116ed79361c4a28c7dad76
PrototypingInterfaces <git@PrototypingInterfaces>
2013-12-17 14:37:35 +0100

    .

--- test.sh
+++ test.sh
@@ -2,9 +2,9 @@
 # Update Subtree For Branches and Push
 
 version=`cat version.txt`
-versiontext="$version jay"
-touch "$version".xml
+versiontext="(for $version)"
+# touch "$version".xml
 
-convert tmpimage.png -gravity SouthEast -font Armata-Regular -pointsize 11 -draw "text 0,0 '$versiontext'" ausgabe.png
+convert template.png -gravity SouthEast -font Armata-Regular -pointsize 11 -draw "text 0,0 '$versiontext'" ausgabe.png
 
-cp ausgabe.png /var/www/PrototypingInterfaces/ausgabe.png
+mv ausgabe.png /var/www/PrototypingInterfaces/ausgabe.png
